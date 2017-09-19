50dbe194aadfc1167a3f1046ea13c888ffe88cae
ghost <ghost@konpa.ku>
2017-09-19 23:48:10 +0000

    0.0.1 - 4c3589b35c3d916e7dbb3fae1d1db4f9049e03cc

---
+++ implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["input_linux_sys"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"struct\" href=\"input_linux_sys/struct.input_absinfo.html\" title=\"struct input_linux_sys::input_absinfo\">input_absinfo</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
