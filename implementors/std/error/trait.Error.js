faebd80c50abc02a50a4c5b903ad71836b937d46
ghost <ghost@konpa.ku>
2017-09-22 19:13:09 +0000

    0.1.3 - 3eab98d947e30ffa9579adc10ae3c7a2d7c75d0c

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["input_linux_sys"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for Void","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for Error","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for Errno",];
+implementors["input_linux_sys"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for Void","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"input_linux_sys/enum.Error.html\" title=\"enum input_linux_sys::Error\">Error</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"input_linux_sys/enum.Errno.html\" title=\"enum input_linux_sys::Errno\">Errno</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
