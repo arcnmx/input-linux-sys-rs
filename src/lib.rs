06dbe8520dd4584344bebc1578783e56ded7044b
arcnmx <arcnmx@users.noreply.github.com>
2024-07-19 10:52:54 -0700

    chore: bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/input-linux-sys/0.8.1/")]
+#![doc(html_root_url = "https://docs.rs/input-linux-sys/0.8.2/")]
 
 pub use nix::{Error, Result};
 pub use nix::errno::Errno;
